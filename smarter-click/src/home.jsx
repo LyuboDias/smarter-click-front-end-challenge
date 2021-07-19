f8f5461ef25ec846b30d30321ee929567c92aa77
Lyubomir Dias <lyubomirdias@gmail.com>
2021-07-19 18:11:15 +0100

    styled of front end challenge finished

--- smarter-click/src/home.jsx
+++ smarter-click/src/home.jsx
@@ -7,6 +7,13 @@ function Home() {
       <div className="logo-div">
         <img src={logo} alt="logo" />
       </div>
+      <div>
+        <svg class="arrow">
+          <path class="a1" d="M0 0 L30 22 L60 0"></path>
+          <path class="a2" d="M0 20 L30 42 L60 20"></path>
+          <path class="a3" d="M0 40 L30 62 L60 40"></path>
+        </svg>
+      </div>
     </div>
   );
 }
