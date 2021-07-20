ffda0864d817f5e5126b12f5ffa8c661b79ea807
Lyubomir Dias <lyubomirdias@gmail.com>
2021-07-20 15:25:21 +0100

    update

---
+++ db.php
@@ -0,0 +1,7 @@
+
+<?php
+// mock array
+$array = array('key1' => "6f564129305a9f1fe6bdee84336399a6",
+'key2' => "560eca685ec857296bdd29b87cb8bbf0",
+'key3' => "33199e9d4570f28fc9474579e5edf1be")
+?>
